e2df0ae518d5b8a15ea15e98d73f963f21ef680c
Aaditya Dubey <ad2340033@gmail.com>
2025-02-13 01:55:43 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { fadeIn } from "@/lib/motion";
+import { fadeIn } from "./lib/motion";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";

dfbc36b1d18cabf9dc2ae948de1e643892b3f465
Aaditya Dubey <ad2340033@gmail.com>
2025-02-13 01:57:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
+import Home from "./pages/Home";
 
 function Router() {
   return (
